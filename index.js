4d5856213b5cd7660d5f05cef3e09932b321a322
Sim P <anomis.p@gmail.com>
2024-03-13 21:12:39 +0100

    bulb on/off toggle script

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+/*Vytvořte si repozitář ze šablony cviceni-zarovka se stránkou, která zobrazuje obyčejnou žárovku.
+Pomocí JavaScriptu zařiďte, aby se při stisknutí libovolné klávesy na stránce přidala k elementu žárovky CSS třída bulb--on. 
+Žárovka by se takto měla rozsvítit.
+Vylepšete program tak, aby na následné stisknutí libovolné klávesy žárovka opět zhasnula. 
+Opakovaným mačkáním kláves ji tak můžeme rozsvěcovat a zhasínat.
+*/
 console.log('funguju!');
+
+const zarovka = document.querySelector('.bulb')
+document.addEventListener("keydown", ()=> {
+    zarovka.classList.toggle('bulb--on')
+}) 
+
